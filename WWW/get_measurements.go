7e7f4a7e8bd95467dbd8f8be37c614a79a05de81
Andrzej Rafalski <anraf1001@gmail.com>
2023-01-21 17:48:39 +0100

    Use map as measurements container

--- WWW/get_measurements.go
+++ WWW/get_measurements.go
@@ -12,15 +12,6 @@ type Measurement struct {
 	Unit  string  `json:"unit"`
 }
 
-type MeasCollection struct {
-	Temperature Measurement `json:"temperature"`
-	Humidity    Measurement `json:"humidity"`
-	Pressure    Measurement `json:"pressure"`
-	Roll        Measurement `json:"roll"`
-	Pitch       Measurement `json:"pitch"`
-	Yaw         Measurement `json:"yaw"`
-}
-
 func get_measurements(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/get_measurements" {
 		http.Error(w, "404 not found", http.StatusNotFound)
@@ -46,15 +37,13 @@ func get_measurements(w http.ResponseWriter, r *http.Request) {
 	var measurements_list []float32
 	json.Unmarshal(output, &measurements_list)
 
-	temperature := Measurement{Value: measurements_list[0], Unit: "C"}
-	humidity := Measurement{Value: measurements_list[1], Unit: "%"}
-	pressure := Measurement{Value: measurements_list[2], Unit: "hPa"}
-
-	roll_position := Measurement{Value: measurements_list[3], Unit: "deg"}
-	pitch_position := Measurement{Value: measurements_list[4], Unit: "deg"}
-	yaw_position := Measurement{Value: measurements_list[5], Unit: "deg"}
-
-	resp := MeasCollection{Temperature: temperature, Humidity: humidity, Pressure: pressure, Roll: roll_position, Pitch: pitch_position, Yaw: yaw_position}
+	resp := make(map[string]Measurement)
+	resp["temperature"] = Measurement{Value: measurements_list[0], Unit: "C"}
+	resp["humidity"] = Measurement{Value: measurements_list[1], Unit: "%"}
+	resp["pressure"] = Measurement{Value: measurements_list[2], Unit: "hPa"}
+	resp["roll"] = Measurement{Value: measurements_list[3], Unit: "deg"}
+	resp["pitch"] = Measurement{Value: measurements_list[4], Unit: "deg"}
+	resp["yaw"] = Measurement{Value: measurements_list[5], Unit: "deg"}
 
 	jsonResp, err := json.Marshal(resp)
 	if err != nil {
